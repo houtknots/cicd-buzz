941e943772d554cff81ab1541df78f1556cd3972
houtknots <github@virtrobot.com>
2023-01-04 20:30:55 +0100

    Add greetings

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 
 def generate_buzz():
     page = '<html><body><h1>'
+    page += "</br>Greetings from Jayce Houtman & Samir Mehanovic!</br>"
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
